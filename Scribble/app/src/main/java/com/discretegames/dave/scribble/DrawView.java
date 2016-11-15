3197a30cd72aa0a14418fa1472d9f8e847a39269
coderofgames <dave@discrete-hosting.co.uk>
2016-11-15 03:24:09 +0000

    initial commit
    initial commit

---
+++ Scribble/app/src/main/java/com/discretegames/dave/scribble/DrawView.java
@@ -0,0 +1,74 @@
+package com.discretegames.dave.scribble;
+
+/**
+ * Created by dave on 15/11/2016.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+import android.util.AttributeSet;
+import android.view.View.OnTouchListener;
+
+
+public class DrawView extends View implements View.OnTouchListener{
+
+
+
+
+    List<Point> points = new ArrayList<Point>();
+    Paint paint = new Paint();
+
+    public DrawView(Context context) {
+        super(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setOnTouchListener(this);
+        paint.setColor(Color.BLACK);
+
+    }
+
+    public ArrayList GetArrayList()
+    {
+        return (ArrayList)points;
+    }
+
+    public void Clear()
+    {
+        points.clear();
+        this.refreshDrawableState();
+    }
+
+    public DrawView(Context context, AttributeSet attrSet)
+    {
+        super(context,attrSet);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setOnTouchListener(this);
+        paint.setColor(Color.BLACK);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        for (Point point : points) {
+            canvas.drawCircle((float)point.X, (float)point.Y, 2, paint);
+        }
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        Point point = new Point();
+        point.X = event.getX();
+        point.Y = event.getY();
+        points.add(point);
+        invalidate();
+        return true;
+    }
+}
+
+
+
---
+++ Scribble/app/src/main/java/com/discretegames/dave/scribble/MainActivity.java
@@ -0,0 +1,80 @@
+package com.discretegames.dave.scribble;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.graphics.Color;
+import android.widget.Button;
+import android.content.Context;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button button2;
+    DrawView drawView;
+    DollarRecognizer dollarRecognizer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+        //setSupportActionBar(toolbar);
+
+        button  = (Button) findViewById(R.id.button1);
+
+        button2  = (Button) findViewById(R.id.button2);
+
+       // drawView = new DrawView(this);
+        drawView = (DrawView) findViewById(R.id.drawView1);
+        drawView.setBackgroundColor(Color.WHITE);
+        //setContentView(drawView);
+        drawView.requestFocus();
+
+
+        dollarRecognizer = new DollarRecognizer();
+
+    }
+    public void button1_click(View view)
+    {
+        RecognizerResults results = dollarRecognizer.Recognize(drawView.GetArrayList());
+
+        Context context = getApplicationContext();
+        CharSequence text =  results.mName + "  " + results.mScore + "   " + results.mOtherInfo;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void button2_click(View view)
+    {
+     drawView.Clear();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
